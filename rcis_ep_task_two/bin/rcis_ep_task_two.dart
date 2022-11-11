49a9ff49014ed1a8d03f33b3023fa556d2ae25d6
Чарынчык Айдан Валерьевич <117069700+Aydan-Charynchyk-621@users.noreply.github.com>
2022-11-12 01:10:09 +0700

    Update rcis_ep_task_two.dart

--- rcis_ep_task_two/bin/rcis_ep_task_two.dart
+++ rcis_ep_task_two/bin/rcis_ep_task_two.dart
@@ -1,5 +1,21 @@
-import 'package:rcis_ep_task_two/rcis_ep_task_two.dart' as rcis_ep_task_two;
+import 'dart:io';
+import 'dart:math';
 
-void main(List<String> arguments) {
-  print('Hello world: ${rcis_ep_task_two.calculate()}!');
-}
+void main() {
+
+//Задание 1
+//
+//var nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100];
+//nums.sort((b, a) => a.compareTo(b));
+//nums = nums.where((w) => w % 3 == 1).toList();
+//print(nums);
+//}
+//
+//
+//Задание 2
+//
+//
+//var list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+//list = list.where((w) => w % 2!= 0).toList();
+//print(list);
+//}
