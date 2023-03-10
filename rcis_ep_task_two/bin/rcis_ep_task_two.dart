a8120b7f8ea2bb65bda6239daaa2ed7de205b650
Charynchyk <117069700+Aydan-Charynchyk-621@users.noreply.github.com>
2023-03-10 16:58:42 +0700

    2.1 (1)

--- rcis_ep_task_two/bin/rcis_ep_task_two.dart
+++ rcis_ep_task_two/bin/rcis_ep_task_two.dart
@@ -1,49 +1,54 @@
-import 'dart:io';
-import 'dart:math';
+namespace ConsoleApplication1;
 
-void main() {
+public class Program
+{
+    public class Student
+    {
+        private string surname;
+        private DateTime dateOfBirth;
+        private int group;
+        private int[] progress = {};
 
-//Задание 1
-/*
-var nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100];
-nums.sort((b, a) => a.compareTo(b));
-nums = nums.where((w) => w % 3 == 1).toList();
-print(nums);
+
+        public void Info()
+        {
+            Console.WriteLine("Фамилия: {0}\nДата Рождения: {1}\nГруппа: {2}", surname, dateOfBirth, group);
+            foreach(int arr in progress)
+            Console.Write(arr + " ");
+        }
+        
+        public void Changeinfo(string surname)
+        {
+            this.surname = surname;
+        }
+        public void Changeinfo(string surname, DateTime dateOfBirth)
+        {
+            this.surname = surname;
+            this.dateOfBirth = dateOfBirth;
+        }
+        public void Changeinfo(string surname, DateTime dateOfBirth, int group)
+        {
+            this.surname = surname;
+            this.dateOfBirth = dateOfBirth;
+            this.group = group;
+        }
+
+        public Student(string surname, DateTime dateOfBirth, int group, int [] progress)
+        {
+            this.surname = surname;
+            this.dateOfBirth = dateOfBirth;
+            this.group = group;
+            this.progress = progress;
+        }
+    }
+
+    static void Main()
+    {
+        Student myInfo= new Student("Maksimov", new DateTime(1991, 01, 30), 714, new int [5] {4, 3, 4, 5, 2});
+
+        myInfo.Changeinfo(surname: "Charynchyk", dateOfBirth: new DateTime(2004, 11, 27), group: 621);
+        
+        myInfo.Info();
+    }
 }
-/*
-//
-//Задание 2
-//
-/*
-var list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-list = list.where((w) => w % 2!= 0).toList();
-print(list);
-}
-/*
-//Задание 3 
-//
-void main() {
-  int a = 5;
-   List<List<int>> matrissa = [[1, 1, 1, 1, 1],
-                             [1, 1, 1, 1, 1],
-                             [1, 1, 1, 1, 1],
-                             [1, 1, 1, 1, 1],
-                             [1, 1, 1, 1, 1]];
-   for (int i = 0; i < a; i++){
-     for (int j = 0; j < a; j++){
-       if (i == 0 || j == 0){
-         matrissa[i][j] = 1;
-       }
-     else{
-        matrissa[i][j] = matrissa[i - 1][j] + matrissa[i][j - 1];
-       }
-     }
-   }
-   for (int i = 0; i < a; i++){
-   for (int j = 0; j < a; j++){
-       stdout.write("${matrissa[i][j]}   ");
-     }
-     print("");
-   }
-}
-/*
+
